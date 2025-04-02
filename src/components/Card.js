ffd7c74f841a7f69d0b8579a57bbd5363ea171e7
Casey <dinnerdoggy@gmail.com>
2025-04-01 20:09:00 -0500

    .

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+'use client';
+
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+function TaskCard() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>Some quick example text to build on the card title and make up the bulk of the cards content.</Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default TaskCard;
